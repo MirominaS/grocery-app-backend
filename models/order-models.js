8aff28adb1d0fa3f1f7188944a54d7259dbd535b
MirominaS <miromin-im19063@stu.kln.ac.lk>
2026-03-26 01:43:03 +0530

    models are created to store order_data

---
+++ models/order-models.js
@@ -0,0 +1,11 @@
+export const order_data = [
+  {
+    id: 1,
+    customer_name: "David",
+    phone_no: "1234567890",
+    address: "Colombo",
+    total: 3.2,
+    items: [{ product_id: 1, quantity: 2 }, { product_id: 2, quantity: 1 }],
+    created_at: new Date()
+  }
+]
