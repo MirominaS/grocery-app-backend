3778617f35aac7883622efeb68345d5e5f0a865f
MirominaS <miromin-im19063@stu.kln.ac.lk>
2026-03-26 01:44:20 +0530

    api created for create order

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 import express, { json } from 'express'
 import { product_data } from './models/product-models.js';
 import productRouter from './routes/product-routes.js'
-
+import orderRouter from './routes/order-routes.js'
 const app = express()
 const PORT = 3000;
 
 app.use(json());
 app.use("/grocery",productRouter)
+app.use("/grocery",orderRouter)
 
 app.listen(PORT, () => {
     console.log(`grocery-app-backend is running on ${PORT}.`);
---
+++ routes/order-routes.js
@@ -0,0 +1,9 @@
+import express from 'express'
+import { createOrderController } from '../controllers/order-controller.js';
+
+
+const router = express.Router();
+
+router.post("/orders",createOrderController)
+
+export default router;
