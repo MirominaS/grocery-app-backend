dd4cc71b897121dc44ec7e20888912bfb7fa1032
MirominaS <miromin-im19063@stu.kln.ac.lk>
2026-03-26 00:40:46 +0530

    api created for products

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
-import express from 'express'
-import { product_data } from './models/product_models.js';
-
+import express, { json } from 'express'
+import { product_data } from './models/product-models.js';
+import productRouter from './routes/product-routes.js'
 
 const app = express()
 const PORT = 3000;
 
-app.get('/',(req, res) => {
-    res.send("Hello world")
-})
-
-app.get('/products',(req,res) => {
-    console.log(product_data);
-    res.json(product_data)
-})
+app.use(json());
+app.use("/grocery",productRouter)
 
 app.listen(PORT, () => {
     console.log(`grocery-app-backend is running on ${PORT}.`);
