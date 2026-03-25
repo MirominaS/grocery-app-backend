18eff5ddf842847643cea01eecc5bba2fbe6d791
MirominaS <miromin-im19063@stu.kln.ac.lk>
2026-03-26 00:33:00 +0530

    loggers file created for error handling

---
+++ utils/loggers.js
@@ -0,0 +1,11 @@
+const current_date = new Date().toISOString()
+
+export const logger = (status) => {
+    console.log(`[${current_date}] : `, status)
+}
+
+export const errorLogger = (error_status) => {
+    console.log(`[${current_date}] : `, error_status)
+}
+
+
