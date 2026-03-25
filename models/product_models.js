ab4974036fe1d055d227cd30f70a8e22c40b0dd4
MirominaS <miromin-im19063@stu.kln.ac.lk>
2026-03-25 23:45:29 +0530

    models created to store product data

---
+++ models/product_models.js
@@ -0,0 +1,23 @@
+export const product_data = [
+    { 
+        id: 0, 
+        name: "Apple",
+        price: 120, 
+        category: "Fruits", 
+        stock: 100 
+    },
+    { 
+        id: 1, 
+        name: "Milk", 
+        price: 0.8, 
+        category: "Dairy", 
+        stock: 50 
+    },
+    { 
+        id: 2, 
+        name: "Bread", 
+        price: 1.0, 
+        category: "Bakery", 
+        stock: 30 
+    }
+]
