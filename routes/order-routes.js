3e9e54187c239e6e490ccea80d2af67cfb0731a3
MirominaS <miromin-im19063@stu.kln.ac.lk>
2026-03-26 23:59:15 +0530

    formatted page

--- routes/order-routes.js
+++ routes/order-routes.js
@@ -1,7 +1,6 @@
 import express from 'express'
 import { createOrderController, getOrdersController } from '../controllers/order-controller.js';
 
-
 const router = express.Router();
 
 router.post("/orders",createOrderController)
