096c3f386af904228397f8838ebe7ca21cb20dc3
MirominaS <miromin-im19063@stu.kln.ac.lk>
2026-03-26 19:14:31 +0530

    installed cors and updated the index.js

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ import express, { json } from 'express'
 import { product_data } from './models/product-models.js';
 import productRouter from './routes/product-routes.js'
 import orderRouter from './routes/order-routes.js'
+import cors from 'cors'
+
 const app = express()
 const PORT = 3000;
+
+app.use(cors({
+    origin:"http://localhost:5173"
+}))
 
 app.use(json());
 app.use("/grocery",productRouter)
@@ -12,3 +18,4 @@ app.use("/grocery",orderRouter)
 app.listen(PORT, () => {
     console.log(`grocery-app-backend is running on ${PORT}.`);
 })
+
